1f4ba813dae15625fc9f289522b5b587e53ebd45
vanchuong119 <vanchuong119@gmail.com>
2017-10-16 20:34:52 +0700

    [CHUONG] bai tap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO toi la Chuong
+        System.out.println("Chuong");
     }
 }
