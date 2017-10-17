f076891e034d4457b960066105afbdca0121a675
vanchuong119 <vanchuong119@gmail.com>
2017-10-17 21:05:35 +0700

    [CHUONG] Bai Tap 1 - So 2

---
+++ app/src/main/java/com/example/sony/training/testgit.java
@@ -0,0 +1,11 @@
+package com.example.sony.training;
+
+/**
+ * Created by Administrator on 10/16/17.
+ */
+
+public class testgit {
+    public testgit(){
+        System.out.println("TEST");
+    }
+}
