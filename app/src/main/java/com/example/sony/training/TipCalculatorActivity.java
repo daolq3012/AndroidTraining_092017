1e7f37a029867ba58332232cb8298db430cac080
thanhthang <huynhthanhthang1296@gmail.com>
2017-10-18 20:10:41 +0700

    [ThanhThang] Android Buoi 2 Bai tap 1

---
+++ app/src/main/java/com/example/sony/training/TipCalculatorActivity.java
@@ -0,0 +1,81 @@
+package com.example.sony.training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+public class TipCalculatorActivity extends AppCompatActivity implements TextView.OnEditorActionListener, View.OnClickListener {
+    private EditText billAmountEditText;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private Button percentUpButton;
+    private Button percentDownButton;
+    private float tipPercent = 0.1f;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        percentDownButton = (Button) findViewById(R.id.percentDownButton);
+        percentUpButton = (Button) findViewById(R.id.percentUpButton);
+
+        billAmountEditText.setOnEditorActionListener(this);
+
+        percentDownButton.setOnClickListener(this);
+        percentUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED){
+            calculateAndDisplay();
+        }
+        return false;
+    }
+
+    public void calculateAndDisplay(){
+        //get the bill
+        String billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if(billAmountString.equals("")){
+            billAmount = 0;
+        } else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+        //calculate
+        float tipAmount = billAmount * tipPercent;
+        float totalAmount = billAmount + tipAmount;
+        //display
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.percentDownButton:
+                tipPercent = tipPercent - .01f;
+                calculateAndDisplay();
+                break;
+            case R.id.percentUpButton:
+                tipPercent = tipPercent + .01f;
+                calculateAndDisplay();
+                break;
+        }
+    }
+}
