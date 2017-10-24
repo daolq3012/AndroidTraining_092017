d7886297f7a6f8e8928bfba5b49fa03b9c0d7d33
truyenz <tdtruyen92lmq@gmail.com>
2017-10-24 17:31:25 +0700

    [Truyen] Android - Buoi 3 - Bai tap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTxtUserName;
+    private EditText mEdtUserName;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_2);
+
+        mTxtUserName = (TextView) findViewById(R.id.txtUserName);
+        mEdtUserName = (EditText) findViewById(R.id.edtUserName);
+        mSpinner = (Spinner) findViewById(R.id.spinner_split);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_split,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
     }
 }
