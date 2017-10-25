4345fb1ca26e89e159dfed38fac499a03308434d
NguyenNam34 <khacnamdt@gmail.com>
2017-10-25 20:44:04 +0700

    [NamNK] Android Buoi 4 -- Bai tap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button calculatorButton;
+    private TextView mEditTotal;
+    private EditText mEditbill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_buoi4);
+
+        calculatorButton = (Button) findViewById(R.id.calculatorButton);
+        mEditTotal = (TextView) findViewById(R.id.mEditTotal);
+        mEditbill = (EditText) findViewById(R.id.mEditbill);
+
+        calculatorButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.calculatorButton:
+                String input = mEditbill.getText().toString();
+                int so = Integer.parseInt(input)/2;
+                mEditTotal.setText("$"+so);
+                break;
+            case R.id.mTotal:
+                Toast.makeText(this,"Click vao total textview",Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
