7fb99cffd729ca3d8c2d841ef22cfe1b1bc7a3e8
thanhthang <huynhthanhthang1296@gmail.com>
2017-10-25 20:44:20 +0700

    [ThanhThang] Android Buoi 4 Bai 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,28 +2,54 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTxtUN;
-    private EditText mEdtUN;
+import org.xml.sax.Parser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     private Spinner mSpinner;
+    private Button mCalculate;
+    private TextView mTotal;
+    private EditText mBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_2);
+        setContentView(R.layout.layout);
 
-//        mTxtUN = (TextView) findViewById(R.id.txtUN);
-//        mEdtUN = (EditText) findViewById(R.id.editUN);
-        mSpinner = (Spinner) findViewById(R.id.spinner);
+        //widget
+        mCalculate = (Button) findViewById(R.id.btnCalculate);
+        mSpinner = (Spinner) findViewById(R.id.Spn);
+        mTotal = (TextView) findViewById(R.id.txtTotal1);
+        mBill = (EditText) findViewById(R.id.edtBill);
 
+        //Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner, android.R.layout.simple_spinner_item);
+                R.array.spinner1, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner.setAdapter(adapter);
+
+        //Listen
+        mCalculate.setOnClickListener(this);
+        mTotal.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnCalculate:
+                String input = mBill.getText().toString();
+                float total = Float.parseFloat(input) / 2;
+                mTotal.setText("$ "+total);
+                break;
+        }
+    }
+
 }
