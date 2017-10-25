7b9b9426e59d80660c7df653dac9c7785f4e67c2
hoaibaohue <anhvuceo@gmail.com>
2017-10-25 18:01:25 +0700

    Android _Buoi3 _ 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.FocusFinder;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+     private EditText meditusename ;
+    private  EditText PASSWORD ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
