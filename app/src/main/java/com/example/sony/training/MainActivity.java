c07f2ff16428a1040dd9b08cffc1ca3ea322d4ec
truyenz <tdtruyen92lmq@gmail.com>
2017-10-25 20:48:50 +0700

    [Truyen] Android - Buoi 4 - Bai tap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+
+    private Button calculateButton;
+    private TextView mTxtTotalNumber;
+    private EditText mEdtBillAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
+
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        mTxtTotalNumber = (TextView) findViewById(R.id.txtTotalNumber);
+        mEdtBillAmount = (EditText) findViewById(R.id.edtBillAmount);
+
+//        calculateButton.setOnClickListener(this);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String total = mEdtBillAmount.getText().toString();
+                float result = Float.parseFloat(total);
+                mTxtTotalNumber.setText("$"+(result/2));
+                Toast.makeText(MainActivity.this,"Click vao button calculate",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.calculateButton:
+//                String total = mEdtBillAmount.getText().toString();
+//                mTxtTotalNumber.setText(total);
+//                Toast.makeText(this,"Click vao button calculate",Toast.LENGTH_LONG).show();
+//        }
+//    }
+
 }
