3a222362865fbbebdfa50075aa16c721c08c4b75
vanchuong119 <vanchuong119@gmail.com>
2017-10-30 01:40:15 +0700

    [CHUONG] Android - Buoi 4 - Bai tap 4

---
+++ app/src/main/java/com/example/sony/training/LoginForm.java
@@ -0,0 +1,60 @@
+package com.example.sony.training;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginForm extends AppCompatActivity implements View.OnClickListener {
+    private EditText mEdtEmail, mEdtPassword;
+    private Button mBtnLogin;
+    private TextView mTxtRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        initView();
+
+        initListener();
+    }
+
+    private void initView(){
+        mEdtEmail = (EditText) findViewById(R.id.email_EditText);
+        mEdtPassword = (EditText) findViewById(R.id.password_EditText);
+        mBtnLogin = (Button) findViewById(R.id.login_Button);
+        mTxtRegister = (TextView) findViewById(R.id.register_TextView);
+    }
+
+    private void initListener(){
+        mBtnLogin.setOnClickListener(this);
+        mTxtRegister.setOnClickListener(this);
+    }
+
+    private void checkLogin(){
+        String email = mEdtEmail.getText().toString();
+        String password = mEdtPassword.getText().toString();
+        if (email.length() > 6 || password.length() > 6){
+            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Email or Password must > 6 characters.", Toast.LENGTH_SHORT).show();
+            mEdtEmail.setText("");
+            mEdtPassword.setText("");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_Button:
+                checkLogin();
+                break;
+            case R.id.register_TextView:
+                startActivity(new Intent(this,RegisterForm.class));
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sony.training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener */{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
