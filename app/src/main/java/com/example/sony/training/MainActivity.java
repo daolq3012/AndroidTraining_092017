f46772fa581e2c2af104e8fad6c69bd37e0da33f
vanchuong119 <vanchuong119@gmail.com>
2017-10-30 20:14:09 +0700

    [CHUONG] Android - Buoi 4 - Bai tap 4

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.sony.training;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener */{
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
