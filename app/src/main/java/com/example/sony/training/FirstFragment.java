be52f9446e8f64f7f6944e597e3c4ad326f09028
phongchauchp <phongchautechnew@gmail.com>
2017-11-01 22:46:36 +0700

    [PHONGCH]: ---Android--Buoi6-- Baitap1

--- app/src/main/java/com/example/sony/training/FirstFragment.java
+++ app/src/main/java/com/example/sony/training/FirstFragment.java
@@ -2,31 +2,35 @@ package com.example.sony.training;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
+ * to handle interaction events.
  * create an instance of this fragment.
  */
-public class FirstFragment extends Fragment implements View.OnClickListener{
+public class FirstFragment extends Fragment implements View.OnClickListener {
+
+    private RelativeLayout firstFragmentParentLayout;
     private TextView firstTextView;
     private Button firstButton;
 
     private boolean isClicked = false;
+
     private FirstFragmentEventListener firstFragmentEventListener;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if(context instanceof  FirstFragmentEventListener){
+        if (context instanceof FirstFragmentEventListener){
             firstFragmentEventListener = (FirstFragmentEventListener) context;
         }
     }
@@ -36,22 +40,25 @@ public class FirstFragment extends Fragment implements View.OnClickListener{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        firstFragmentParentLayout = (RelativeLayout) view.findViewById(R.id.firstFragmentParentLayout);
         firstTextView = (TextView) view.findViewById(R.id.firstTextView);
         firstButton = (Button) view.findViewById(R.id.firstButton);
+
+        firstButton.setOnClickListener(this);
         return view;
     }
 
     @Override
-    public void onClick(View v) {
-        switch ((v.getId())){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.firstButton:
                 firstFragmentEventListener.onButtonClicked();
-                isClicked =!isClicked;
-                firstTextView.setTextColor(getResources().getColor(isClicked ? android.R.color.background_dark: R.color.colorAccent));
+                isClicked = !isClicked;
+                firstTextView.setTextColor(getResources().getColor(isClicked ? android.R.color.black : R.color.colorAccent));
                 break;
             default:
                 break;
         }
-
     }
 }
--- app/src/main/java/com/example/sony/training/SecondFragment.java
+++ app/src/main/java/com/example/sony/training/SecondFragment.java
@@ -1,26 +1,13 @@
 package com.example.sony.training;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * create an instance of this fragment.
- */
-public class SecondFragment extends android.app.Fragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+public class SecondFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,5 +15,4 @@ public class SecondFragment extends android.app.Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
-
 }
