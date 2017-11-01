3757657e143fb38ab7abaf9e6a868031a21d657d
daolq <le.quang.dao@framgia.com>
2017-11-01 20:52:21 +0700

    chapter 9 bai tap 2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,34 +1,24 @@
 package com.example.sony.training;
 
 import android.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements FirstFragmentEventListener{
+import com.example.sony.training.adapter.TestPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager testViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirstFragment firstFragment = new FirstFragment();
-        getFragmentManager().beginTransaction().add(R.id.container_first_fragment,firstFragment).commitAllowingStateLoss();
+        testViewPager = (ViewPager) findViewById(R.id.testViewPager);
 
-        SecondFragment secondFragment = new SecondFragment();
-        getFragmentManager().beginTransaction().add(R.id.container_second_fragment, secondFragment).commitAllowingStateLoss();
-    }
-
-    @Override
-    public void onButtonClicked() {
-        if (isFirstFragmentAdded()){
-            return;
-        }
-        FirstFragment firstFragment = new FirstFragment();
-        getFragmentManager().beginTransaction().replace(R.id.container_second_fragment, firstFragment).commitAllowingStateLoss();
-    }
-
-    private boolean isFirstFragmentAdded(){
-        Fragment fragment = getFragmentManager().findFragmentById(R.id.container_second_fragment);
-        return fragment instanceof FirstFragment;
+        TestPagerAdapter adapter = new TestPagerAdapter(getSupportFragmentManager());
+        testViewPager.setAdapter(adapter);
     }
 }
