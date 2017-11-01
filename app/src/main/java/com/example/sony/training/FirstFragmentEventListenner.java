de84f7a1241cbfa1466a6f3a9ff2aa95af9861f2
thanhthang <huynhthanhthang1296@gmail.com>
2017-11-01 20:29:27 +0700

    Android buoi 6 baitap 1

---
+++ app/src/main/java/com/example/sony/training/FirstFragmentEventListenner.java
@@ -0,0 +1,10 @@
+package com.example.sony.training;
+
+/**
+ * Created by ThanhThang on 01/11/2017.
+ */
+
+public interface FirstFragmentEventListenner {
+        void  onButtonClick();
+
+}
--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,29 +1,35 @@
 package com.example.sony.training;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTxtUN;
-    private EditText mEdtUN;
-    private Spinner mSpinner;
+public class MainActivity extends AppCompatActivity implements FirstFragmentEventListenner{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_2);
+        setContentView(R.layout.activity_main);
 
-//        mTxtUN = (TextView) findViewById(R.id.txtUN);
-//        mEdtUN = (EditText) findViewById(R.id.editUN);
-        mSpinner = (Spinner) findViewById(R.id.spinner);
+        FisrtFragment fisrtFragment = new FisrtFragment();
+        getFragmentManager().beginTransaction().add(R.id.content_first_fragment, fisrtFragment).commitAllowingStateLoss();
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(adapter);
+        SecondFragment secondFragment = new SecondFragment();
+        getFragmentManager().beginTransaction().add(R.id.content_second_fragment, secondFragment).commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onButtonClick() {
+        if (isFisrtFragmentAdded()){
+            return;
+        }
+        FisrtFragment fisrtFragment = new FisrtFragment();
+        getFragmentManager().beginTransaction().add(R.id.content_second_fragment,fisrtFragment).commitAllowingStateLoss();
+
+    }
+
+    public boolean isFisrtFragmentAdded() {
+        android.app.Fragment fragment = getFragmentManager().findFragmentById(R.id.content_second_fragment);
+        return fragment instanceof FisrtFragment;
     }
 }
