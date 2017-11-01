5f863f924ad7dd9da1502f35e815c73e70f5cf5e
truyenz <tdtruyen92lmq@gmail.com>
2017-11-01 20:49:40 +0700

    commit

--- app/src/main/java/com/example/sony/training/FirstFragment.java
+++ app/src/main/java/com/example/sony/training/FirstFragment.java
@@ -3,7 +3,7 @@ package com.example.sony.training;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,14 +24,6 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
 
     private FirstFragmentEventListener firstFragmentEventListener;
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if(context instanceof FirstFragmentEventListener) {
-            firstFragmentEventListener = (FirstFragmentEventListener) context;
-        }
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,7 +31,6 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
         mFirstTextView = (TextView) view.findViewById(R.id.firstTextView);
         mFirstButton = (Button) view.findViewById(R.id.firstButton);
-        relativeLayout = (RelativeLayout) view.findViewById(R.id.container_second_fragment);
         mFirstButton.setOnClickListener(this);
         return view;
     }
@@ -48,7 +39,6 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.firstButton:
-                firstFragmentEventListener.onButtonClicked();
                 isClicked = !isClicked;
                 mFirstTextView.setTextColor(getResources().getColor(isClicked ? R.color.colorAccent : R.color.colorPrimaryDark));
                 break;
