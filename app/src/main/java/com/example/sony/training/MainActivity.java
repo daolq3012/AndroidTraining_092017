515ae705e3956ba6ba670c6a23addc6deaa41aa1
thanhthang <huynhthanhthang1296@gmail.com>
2017-11-03 19:47:12 +0700

    Android buoi 7 baitap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,22 +1,46 @@
 package com.example.sony.training;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.sony.training.Adapter.TestPagerAdapter;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements FirstFragmentEventListenner{
     private ViewPager testViewPager;
-
+    private BottomBar bottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         testViewPager = (ViewPager) findViewById(R.id.testViewPager);
-        TestPagerAdapter testPagerAdapter = new TestPagerAdapter(getSupportFragmentManager());
+        final TestPagerAdapter testPagerAdapter = new TestPagerAdapter(getSupportFragmentManager());
         testViewPager.setAdapter(testPagerAdapter);
+
+        bottomBar = (BottomBar) findViewById(R.id.bottom_bar);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.tab1:
+                        testViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.tab2:
+                        testViewPager.setCurrentItem(1);
+                        break;
+                }
+
+            }
+        });
     }
 
+    @Override
+    public void onButtonClick() {
+
+    }
 }
