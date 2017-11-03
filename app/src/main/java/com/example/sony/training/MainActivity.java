ca141039fad827bd7c29250fdd7e06e3c4521872
daolq <le.quang.dao@framgia.com>
2017-11-03 16:25:37 +0700

    Android Buoi 7 Bai 2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.sony.training;
 
-
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
-
 import com.example.sony.training.adapter.LoginRegisterPagerAdapter;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity implements FirstFragmentEventListener{
     private ViewPager mVpLoginRegister;
     private LoginRegisterPagerAdapter loginRegisterPagerAdapter;
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,24 @@ public class MainActivity extends AppCompatActivity implements FirstFragmentEven
         setContentView(R.layout.activity_main);
 
         mVpLoginRegister = (ViewPager) findViewById(R.id.loginRegisterViewPager);
+        mBottomBar = (BottomBar) findViewById(R.id.bottom_navigation);
+
         loginRegisterPagerAdapter = new LoginRegisterPagerAdapter(getSupportFragmentManager());
         mVpLoginRegister.setAdapter(loginRegisterPagerAdapter);
+
+        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId) {
+                    case R.id.tab_login:
+                        mVpLoginRegister.setCurrentItem(0);
+                        break;
+                    case R.id.tab_register:
+                        mVpLoginRegister.setCurrentItem(1);
+                        break;
+                }
+            }
+        });
     }
 
     @Override
