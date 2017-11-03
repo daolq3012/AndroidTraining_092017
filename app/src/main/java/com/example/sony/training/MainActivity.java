a7698b64f5d7d7a864ef3e921ae98e7e349b8461
thanhthang <huynhthanhthang1296@gmail.com>
2017-11-03 12:46:27 +0700

    Android buoi 6 baitap 2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,35 +1,22 @@
 package com.example.sony.training;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements FirstFragmentEventListenner{
+import com.example.sony.training.Adapter.TestPagerAdapter;
 
+public class MainActivity extends AppCompatActivity{
+    private ViewPager testViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FisrtFragment fisrtFragment = new FisrtFragment();
-        getFragmentManager().beginTransaction().add(R.id.content_first_fragment, fisrtFragment).commitAllowingStateLoss();
-
-        SecondFragment secondFragment = new SecondFragment();
-        getFragmentManager().beginTransaction().add(R.id.content_second_fragment, secondFragment).commitAllowingStateLoss();
+        testViewPager = (ViewPager) findViewById(R.id.testViewPager);
+        TestPagerAdapter testPagerAdapter = new TestPagerAdapter(getSupportFragmentManager());
+        testViewPager.setAdapter(testPagerAdapter);
     }
 
-    @Override
-    public void onButtonClick() {
-        if (isFisrtFragmentAdded()){
-            return;
-        }
-        FisrtFragment fisrtFragment = new FisrtFragment();
-        getFragmentManager().beginTransaction().add(R.id.content_second_fragment,fisrtFragment).commitAllowingStateLoss();
-
-    }
-
-    public boolean isFisrtFragmentAdded() {
-        android.app.Fragment fragment = getFragmentManager().findFragmentById(R.id.content_second_fragment);
-        return fragment instanceof FisrtFragment;
-    }
 }
