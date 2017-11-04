19afe9d6c4bbb5958e6172e412ed77eb2288c7dd
vanchuong119 <vanchuong119@gmail.com>
2017-11-04 23:32:19 +0700

    [CHUONG] Android - Buoi 7 - Bai tap 4

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerView mRecyclerView;
+    private List<Item> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(createData());
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void initViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    private List<Item> createData(){
+        data = new ArrayList<>();
+        data.add(new Item("Chuong",20));
+        data.add(new Item("Truyen",20));
+        data.add(new Item("Nguyen",21));
+        data.add(new Item("Nhac",20));
+        data.add(new Item("Trung",20));
+        data.add(new Item("Phuc",20));
+        return data;
     }
 }
---
+++ app/src/main/java/com/example/sony/training/RecyclerViewAdapter.java
@@ -0,0 +1,53 @@
+package com.example.sony.training;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 11/04/17.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+    private List<Item> list = new ArrayList<>();
+
+    public RecyclerViewAdapter(List<Item> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = layoutInflater.inflate(R.layout.item_layout, viewGroup, false);
+        return new RecyclerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder recyclerViewHolder, int i) {
+        recyclerViewHolder.mTxtName.setText(list.get(i).getName());
+        recyclerViewHolder.mTxtAge.setText(list.get(i).getAge()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size()  ;
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTxtName, mTxtAge;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            mTxtName = (TextView) itemView.findViewById(R.id.nameTextView);
+            mTxtAge = (TextView) itemView.findViewById(R.id.ageTextView);
+        }
+    }
+}
