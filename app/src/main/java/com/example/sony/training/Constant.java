c0ca5e182fde940422628ab49622790161156abb
thanhthang <huynhthanhthang1296@gmail.com>
2017-11-05 20:47:02 +0700

    [ThanhThang] Android buoi 7 bai 2

---
+++ app/src/main/java/com/example/sony/training/Constant.java
@@ -0,0 +1,9 @@
+package com.example.sony.training;
+
+/**
+ * Created by ThanhThang on 03/11/2017.
+ */
+
+public final class Constant {
+    public static final String  KEY_DATA_INPUT = "KEY_DATA_INPUT";
+}
