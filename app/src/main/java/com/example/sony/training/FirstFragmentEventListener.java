5f9bd2b17ed5027bbb2c2528d096f7a36c8b8e34
phongchauchp <phongchautechnew@gmail.com>
2017-11-06 18:27:41 +0700

    [PHONGCH]: ---Android--Buoi7-- TEST

--- app/src/main/java/com/example/sony/training/FirstFragmentEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.sony.training;
-
-/**
- * Created by phong on 11/01/17.
- */
-
-public interface FirstFragmentEventListener {
-    void onButtonClicked();
-}
--- app/src/main/java/com/example/sony/training/LoginActivity.java
+++ app/src/main/java/com/example/sony/training/LoginActivity.java
@@ -12,10 +12,10 @@ import android.widget.ProgressBar;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String KEY_USER_TO_MAIN = "KEY_USER_TO_MAIN";
+    public static final String KEY_EMAIL_TO_MAIN = "KEY_EMAIL_TO_MAIN";
     public static final String KEY_PASSWORD_TO_MAIN = "KEY_PASSWORD_TO_MAIN";
 
-    public static final String KEY_USER_FROM_REGISTER = "KEY_USER_FROM_REGISTER";
+    public static final String KEY_EMAIL_FROM_REGISTER = "KEY_EMAIL_FROM_REGISTER";
 
     public static final int REQUEST_CODE_REGISTER = 1;
 
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         // when process we must have sometime
 
         // get data
-        String userName = edtEmail.getText().toString().trim();
+        String email = edtEmail.getText().toString().trim();
         String password = edtPass.getText().toString().trim();
 
         // password empty
@@ -77,8 +77,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             error = true;
         }
 
-        // username empty
-        if (TextUtils.isEmpty(userName)) {
+        // email empty
+        if (TextUtils.isEmpty(email)) {
             edtEmail.requestFocus();
             edtEmail.setError(context.getResources().getString(R.string.error_field_required));
             error = true;
@@ -92,7 +92,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(context, MainActivity.class);
 
             // send data if need
-            intent.putExtra(KEY_USER_TO_MAIN, userName);
+            intent.putExtra(KEY_EMAIL_TO_MAIN, email);
             intent.putExtra(KEY_PASSWORD_TO_MAIN, password);
 
             // start Main Activity
@@ -110,8 +110,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_CODE_REGISTER && resultCode == Activity.RESULT_OK) {
-            String userName = data.getStringExtra(KEY_USER_FROM_REGISTER);
-            edtEmail.setText(userName);
+            String email = data.getStringExtra(KEY_EMAIL_FROM_REGISTER);
+            edtEmail.setText(email);
             edtPass.requestFocus();
         }
     }
