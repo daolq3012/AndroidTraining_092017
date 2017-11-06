62ee14254f2910e53e5cc45593519bf16d7c8f39
daolq <le.quang.dao@framgia.com>
2017-11-06 20:33:10 +0700

    Android Buoi 8 Bai 2

---
+++ app/src/main/java/com/example/sony/training/User.java
@@ -0,0 +1,23 @@
+package com.example.sony.training;
+
+/**
+ * Created by daolq on 11/6/17.
+ */
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
