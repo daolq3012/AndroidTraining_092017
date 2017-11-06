2fd8840a40cc2bea0db0273edcc092b9829f6e8c
truyenz <tdtruyen92lmq@gmail.com>
2017-11-06 19:48:14 +0700

    [Truyen] Android - Buoi 8 - Bai tap 1

---
+++ app/src/main/java/com/example/sony/training/Constants.java
@@ -0,0 +1,10 @@
+package com.example.sony.training;
+
+/**
+ * Created by Admin on 11/06/17.
+ */
+
+public class Constants {
+    public static final String SHARE_PREF_NAME = "abc";
+    public static final String SHARE_KEY_PREF_LOGIN = "SHARE_KEY_PREF_LOGIN";
+}
