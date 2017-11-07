c09e2ce457d05e43110a3c85c38232fd2836b0f2
truyenz <tdtruyen92lmq@gmail.com>
2017-11-07 22:51:36 +0700

    [Truyen] Android - Buoi 8 - Bai tap 2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private List<User> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        RecyclerViewAdapter viewAdapter = new RecyclerViewAdapter(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(viewAdapter);
+
+        for (int i = 0; i < 10; i++) {
+            data.add(new User("Truyen "+i,20));
+        }
+        viewAdapter.updateData(data);
     }
 }
