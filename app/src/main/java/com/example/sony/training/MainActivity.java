b7210d15fdd0daf45fbaaa67870ab8fca99e7c0a
hoaibaohue <anhvuceo@gmail.com>
2017-11-08 16:59:58 +0700

    list_san_pham_REcycolerview

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.sony.training;
 
-import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    // gan du lieu vao Recyclerview
+    public void initView(){
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ArrayList<datashop> arrayList = new ArrayList<>();
+        arrayList.add(new datashop(R.drawable.dh2));
+        arrayList.add(new datashop(R.drawable.dh3));
+        arrayList.add(new datashop(R.drawable.dh4));
+        arrayList.add(new datashop(R.drawable.dh5));
+        arrayList.add(new datashop(R.drawable.dh6));
 
+
+
+
+    }
+}
--- app/src/main/java/com/example/sony/training/SecondAvtivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.sony.training;
-
-/**
- * Created by Administrator on 11/03/17.
- */
-
-public class SecondAvtivity
-{
-}
