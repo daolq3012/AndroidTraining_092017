40299982cb13c405ebc5a2949962c3eb0d30ec6a
phongchauchp <phongchautechnew@gmail.com>
2017-11-13 23:33:38 +0700

    [PHONGCH]: --Android - Buoi10 - Btap2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_widgets_calculator);
+        setContentView(R.layout.activity_main);
     }
 }
