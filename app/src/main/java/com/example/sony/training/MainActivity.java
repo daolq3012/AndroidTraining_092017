011c618c09a45e2ccb2553575d97927ac14b68c7
daolq <le.quang.dao@framgia.com>
2017-11-15 19:12:41 +0700

    Android Buoi 11 Bai 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sony.training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.sony.training.receiver.Constant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent intent = new Intent();
+        intent.setAction(Constant.TEST_ACTION);
+        sendBroadcast(intent);
     }
 }
