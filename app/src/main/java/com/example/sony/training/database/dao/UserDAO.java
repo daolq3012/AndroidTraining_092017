bcfead2f44728045b901d4fee7ef4d8e373f29ae
vanchuong119 <vanchuong119@gmail.com>
2017-11-15 21:09:50 +0700

    [CHUONG] Android - Buoi 11 - Bai tap 2

---
+++ app/src/main/java/com/example/sony/training/database/dao/UserDAO.java
@@ -0,0 +1,25 @@
+package com.example.sony.training.database.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import com.example.sony.training.database.entity.UserEntity;
+import java.util.List;
+
+/**
+ * Created by Administrator on 11/15/17.
+ */
+
+@Dao
+public interface UserDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertUser(UserEntity userEntity);
+
+    @Query("SELECT * FROM users")
+    List<UserEntity> getAllUser();
+
+    @Query("SELECT * FROM users WHERE id = :id")
+    UserEntity getUserWithID(int id);
+}
