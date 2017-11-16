483cd651d785f2e1537b1b7dc9f6172ac580131d
thanhthang <huynhthanhthang1296@gmail.com>
2017-11-16 11:18:08 +0700

    [ThanhThang] Android Buoi 11 Bai 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sony.training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setAction(Constant.TEST_ACTION);
+        sendBroadcast(intent);
     }
 }
