fe4965f43b83c14cfe93df6cd335a8469e3b03d3
vanchuong119 <vanchuong119@gmail.com>
2017-11-17 20:19:44 +0700

    [CHUONG] Android - Buoi 12 - Bai tap 1

---
+++ app/src/main/java/com/example/sony/training/service/config/GithubApi.java
@@ -0,0 +1,17 @@
+package com.example.sony.training.service.config;
+
+import com.example.sony.training.service.response.SearchGithubUserResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Administrator on 11/17/17.
+ */
+
+public interface GithubApi {
+    //tu ket noi det github search va tu dong tao object. tra ve object
+    @GET("/search/users")
+    Call<SearchGithubUserResponse> seacrhGithubUser(@Query("per_page") int limit, @Query("q") String keyqword);
+
+}
