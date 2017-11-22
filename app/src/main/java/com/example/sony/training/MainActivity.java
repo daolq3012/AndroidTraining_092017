b8078a8d3abff19f1a1dc56ab016150bbea02646
vanchuong119 <vanchuong119@gmail.com>
2017-11-22 19:29:52 +0700

    [CHUONG] Android - Buoi 13 - Bai tap 1

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.sony.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.example.sony.training.data.ApiService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    private MainContract.Presenter mPresenter;
+    private EditText mEdtUsername, mEdtPassword;
+    private Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        handleEvents();
+
+        mPresenter = new MainPresenter();
+        mPresenter.setView(this);
+        ApiService apiService = new ApiService();
+        mPresenter.setApiService(apiService);
+    }
+
+    private void initViews() {
+        mEdtUsername = (EditText) findViewById(R.id.usernameEdittext);
+        mEdtPassword = (EditText) findViewById(R.id.passwordEdittext);
+        mBtnLogin = (Button) findViewById(R.id.loginButton);
+    }
+
+    private void handleEvents() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPresenter.login();
+            }
+        });
+    }
+
+    @Override
+    public String getUsername() {
+        return mEdtUsername.getText().toString().trim();
+    }
+
+    @Override
+    public String getPassword() {
+        return mEdtPassword.getText().toString().trim();
+    }
+
+    @Override
+    public void showToastLoginSuccess() {
+        Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showToastLoginFail(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/sony/training/MainContract.java
@@ -0,0 +1,27 @@
+package com.example.sony.training;
+
+import com.example.sony.training.data.ApiService;
+
+/**
+ * Created by Administrator on 11/22/17.
+ */
+
+public interface MainContract {
+    interface View{
+        String getUsername();
+
+        String getPassword();
+
+        void showToastLoginSuccess();
+
+        void showToastLoginFail(String message);
+    }
+
+    interface Presenter{
+        void setView(MainContract.View view);
+
+        void setApiService(ApiService apiService);
+
+        void login();
+    }
+}
