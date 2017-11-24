3c18ff9d116a781b10a3f5577bbaf51a8688e9dd
vanchuong119 <vanchuong119@gmail.com>
2017-11-24 18:19:28 +0700

    [CHUONG] Android - Buoi 13 - Bai tap 2

--- app/src/main/java/com/example/sony/training/screen/main/MainPresenter.java
+++ app/src/main/java/com/example/sony/training/screen/main/MainPresenter.java
@@ -1,8 +1,5 @@
 package com.example.sony.training.screen.main;
 
-import android.content.Intent;
-import com.example.sony.training.constant.Constant;
-import com.example.sony.training.screen.listuser.ListUserActivity;
 
 /**
  * Created by Administrator on 11/22/17.
